53ad62e05d68c458209748a35061b75d5666ab01
avihu <avihu2929@gmail.com>
2021-04-20 13:16:21 +0300

    now work with the project's xml file

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -4,16 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Model;
+import view_model.ViewModel;
+
+import java.io.File;
 
 public class MainWindow extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main_window.fxml"));
+
+        Model m = new Model();
+        ViewModel vm = new ViewModel(m);
+        m.addObserver(vm);
+        FXMLLoader fxl = new FXMLLoader();
+      //  BorderPane root =fxl.load(getClass().getResource("main_window.fxml").openStream());
+        Parent root = fxl.load(getClass().getResource("main_window.fxml").openStream());
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 275));
+
+        MainWindowController mwc = fxl.getController();
+        mwc.setViewModel(vm);
+        vm.addObserver(mwc);
+
         primaryStage.show();
+        m.loadXML(new File("resources/playback_small.xml"));
     }
 
 
--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -11,7 +14,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import view_model.ViewModel;
 
+import javax.swing.text.View;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,71 +24,25 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.security.cert.Extension;
+import java.util.Observable;
+import java.util.Observer;
 
-public class MainWindowController {
+public class MainWindowController implements Observer {
 
-    public Label label;
 
-    public void openFile(){
+    ViewModel vm;
+    public Label FeaturesLabel;
 
-        FileChooser fc = new FileChooser();
-        fc.setTitle("Choose flight data file");
-        fc.setInitialDirectory(new File("./resources"));
-        File chosenFile = fc.showOpenDialog(null);
-        if(chosenFile != null){
-            readXML(chosenFile);
-        }
+    public void setViewModel(ViewModel vm){
+        this.vm = vm;
+        //vm.FeaturesLabel.bind(FeaturesLabel.textProperty());
+        FeaturesLabel.textProperty().bind(vm.FeaturesLabel);
     }
 
-    public void readXML(File file){
 
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-        try {
-
-            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            Document doc = db.parse(file);
-            doc.getDocumentElement().normalize();
-
-            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
-            System.out.println("------");
-            NodeList list = doc.getElementsByTagName("chunk");
-            for (int temp = 0; temp < list.getLength(); temp++) {
-                if (list.getLength()/2 == temp){
-                    label.setText(label.getText()+"==============================================\n\n");
-                }
-                Node node = list.item(temp);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element element = (Element) node;
-                    String name = element.getElementsByTagName("name").item(0).getTextContent();
-                    System.out.println("name : " + name);
-                    label.setText(label.getText()+name+"\n");
-
-                    String type = element.getElementsByTagName("type").item(0).getTextContent();
-                    System.out.println("type : " + type);
-                    label.setText(label.getText()+type+"\n");
-
-                    if (element.getElementsByTagName("format").item(0)!=null){
-                        String format = element.getElementsByTagName("format").item(0).getTextContent();
-                        System.out.println("format : " + format);
-                        label.setText(label.getText()+format+"\n");
-                    }
-
-                    String in_node = element.getElementsByTagName("node").item(0).getTextContent();
-                    System.out.println("node : " + in_node);
-                    label.setText(label.getText()+in_node+"\n\n");
-                }
-            }
-
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void update(Observable o, Object arg) {
 
     }
-
 }
